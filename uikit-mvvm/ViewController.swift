abad17d3cfd68f8e67b95eecbc753df2d03120c2
Wildan <wildanalifrio@gmail.com>
2024-12-28 20:29:33 +0700

    Initial Commit

---
+++ uikit-mvvm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  uikit-mvvm
+//
+//  Created by Wildan on 28/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
